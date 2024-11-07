e72cb86bfd4824ab1319eac4a898a3ee09945f18
Ammar <ammar.yassr.33@gmail.com>
2024-11-07 13:56:05 +0200

    format phone number in signup controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -28,7 +28,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
-    phone: req.body.phone,
+    phone: `966${req.body.phone}`,
     password: req.body.password,
   });
 
