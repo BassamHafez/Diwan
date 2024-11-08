93cc3235611110b4e0f56dca7d290e6deff469e1
BassamHafez <bassamhafez790@gmail.com>
2024-11-08 03:23:57 +0200

    working on home design

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,19 @@
+import Hero from "../../Components/Hero/Hero";
+import MainTitle from "../../Components/UI/Words/MainTitle";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Hero />
+      <section className="my-5">
+        <div className="text-center">
+          <MainTitle title="Features " />
+          <h2 className="my-4">Why Choose Our Platform</h2>
+          
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
