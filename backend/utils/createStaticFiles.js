117bfe1b501d104c829b3db560cc88287adc3955
Ammar <ammar.yassr.33@gmail.com>
2024-11-15 23:03:28 +0200

    add functionality to create directories for compounds and estates

--- backend/utils/createStaticFiles.js
+++ backend/utils/createStaticFiles.js
@@ -4,6 +4,8 @@ const path = require("path");
 // Define the paths
 const rootPath = path.join(__dirname, "../", "uploads");
 const usersPath = path.join(rootPath, "users");
+const compoundsPath = path.join(rootPath, "compounds");
+const estatesPath = path.join(rootPath, "estates");
 
 function ensureDirectories() {
   if (!fs.existsSync(rootPath)) {
@@ -14,6 +16,16 @@ function ensureDirectories() {
   if (!fs.existsSync(usersPath)) {
     fs.mkdirSync(usersPath);
     console.log("Created users directory");
+  }
+
+  if (!fs.existsSync(compoundsPath)) {
+    fs.mkdirSync(compoundsPath);
+    console.log("Created compounds directory");
+  }
+
+  if (!fs.existsSync(estatesPath)) {
+    fs.mkdirSync(estatesPath);
+    console.log("Created estates directory");
   }
 }
 
