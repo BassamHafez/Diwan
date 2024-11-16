4608a65bada6bdea50d6680dc873f7b4ecb1a865
Ammar <ammar.yassr.33@gmail.com>
2024-11-16 14:35:42 +0200

    add validation to prevent deletion of compounds with associated estates

--- backend/controllers/compoundController.js
+++ backend/controllers/compoundController.js
@@ -2,6 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 const sharp = require("sharp");
 
 const Compound = require("../models/compoundModel");
+const Estate = require("../models/estateModel");
 const catchAsync = require("../utils/catchAsync");
 const ApiError = require("../utils/ApiError");
 const factory = require("./handlerFactory");
@@ -42,4 +43,26 @@ exports.resizeCompoundImage = catchAsync(async (req, res, next) => {
 
 exports.createCompound = factory.createOne(Compound);
 exports.updateCompound = factory.updateOne(Compound);
-exports.deleteCompound = factory.deleteOne(Compound);
+
+exports.deleteCompound = catchAsync(async (req, res, next) => {
+  const compound = await Compound.findById(req.params.id);
+
+  if (!compound) {
+    return next(new ApiError("No compound found with that ID", 404));
+  }
+
+  const estates = await Estate.find({ compound: compound._id });
+
+  if (estates.length > 0) {
+    return next(
+      new ApiError("Please delete all estates in this compound first", 400)
+    );
+  }
+
+  await Compound.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
