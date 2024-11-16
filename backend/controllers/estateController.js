527b63cc7ffbcaaf91a7f2efe34ad931eeea1d59
Ammar <ammar.yassr.33@gmail.com>
2024-11-16 14:25:21 +0200

    refactor estate creation and update functionality with compound integration

--- backend/controllers/estateController.js
+++ backend/controllers/estateController.js
@@ -2,7 +2,9 @@ const { v4: uuidv4 } = require("uuid");
 const sharp = require("sharp");
 
 const Estate = require("../models/estateModel");
+const Compound = require("../models/compoundModel");
 const catchAsync = require("../utils/catchAsync");
+const ApiError = require("../utils/ApiError");
 const factory = require("./handlerFactory");
 const { uploadSingleImage } = require("../utils/uploadImage");
 
@@ -25,6 +27,7 @@ const compoundPopOptions = [
 
 exports.getAllEstates = factory.getAll(Estate, compoundPopOptions);
 exports.getEstate = factory.getOne(Estate, compoundPopOptions);
+exports.deleteEstate = factory.deleteOne(Estate);
 
 exports.uploadEstateImage = uploadSingleImage("image");
 
@@ -46,6 +49,58 @@ exports.resizeEstateImage = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.createEstate = factory.createOne(Estate);
-exports.updateEstate = factory.updateOne(Estate);
-exports.deleteEstate = factory.deleteOne(Estate);
+exports.createEstate = catchAsync(async (req, res, next) => {
+  let estate;
+
+  if (!req.body.compound) {
+    estate = await Estate.create(req.body);
+  } else {
+    const compound = await Compound.findById(req.body.compound);
+
+    if (!compound) {
+      return next(new ApiError("No compound found with that ID", 404));
+    }
+
+    req.body.region = compound.region;
+    req.body.city = compound.city;
+    if (compound.neighborhood) req.body.neighborhood = compound.neighborhood;
+    if (compound.broker) req.body.broker = compound.broker;
+    if (compound.landlord) req.body.landlord = compound.landlord;
+
+    estate = await Estate.create(req.body);
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: estate,
+  });
+});
+
+exports.updateEstate = catchAsync(async (req, res, next) => {
+  const estate = await Estate.findById(req.params.id);
+
+  if (!estate) {
+    return next(new ApiError("No estate found with that ID", 404));
+  }
+
+  if (estate.compound) {
+    const compound = await Compound.findById(estate.compound);
+
+    req.body.region = compound.region;
+    req.body.city = compound.city;
+    if (compound.neighborhood) req.body.neighborhood = compound.neighborhood;
+    if (compound.broker) req.body.broker = compound.broker;
+    if (compound.landlord) req.body.landlord = compound.landlord;
+  }
+
+  const updatedEstate = await Estate.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: updatedEstate,
+  });
+});
