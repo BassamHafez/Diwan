70b7c54fc55e6d94de08cb58c21e0be36b3ecc4b
Ammar <ammar.yassr.33@gmail.com>
2024-11-19 15:13:33 +0200

    update broker and landlord references

--- backend/models/compoundModel.js
+++ backend/models/compoundModel.js
@@ -35,11 +35,11 @@ const compoundSchema = new mongoose.Schema(
     },
     broker: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Contact",
+      ref: "BrokerContact",
     },
     landlord: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Contact",
+      ref: "LandlordContact",
     },
   },
   { timestamps: true }
