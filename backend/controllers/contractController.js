63936cd4d34a2ba4a0454e16d84ff79ff3fb2d66
Ammar <ammar.yassr.33@gmail.com>
2024-11-20 23:55:41 +0200

    update contract creation logic to set estate status based on contract dates

--- backend/controllers/contractController.js
+++ backend/controllers/contractController.js
@@ -35,15 +35,22 @@ exports.getAllContracts = catchAsync(async (req, res, next) => {
 exports.createContract = catchAsync(async (req, res, next) => {
   const { estateId } = req.params;
 
+  const isActiveContract =
+    new Date(req.body.startDate) < Date.now() &&
+    new Date(req.body.endDate) > Date.now();
+
+  const isFutureContract = new Date(req.body.startDate) > Date.now();
+
   const [estate, contract] = await Promise.all([
-    Estate.findById(estateId),
+    isActiveContract
+      ? Estate.findByIdAndUpdate(estateId, { status: "rented" })
+      : isFutureContract
+      ? Estate.findByIdAndUpdate(estateId, { status: "pending" })
+      : Estate.findById(estateId),
+
     Contract.create({
       ...req.body,
-      status:
-        new Date(req.body.startDate) < Date.now() &&
-        new Date(req.body.endDate) > Date.now()
-          ? "active"
-          : "upcoming",
+      status: isActiveContract ? "active" : "upcoming",
       estate: estateId,
       user: req.user.id,
     }),
