783e04e991bd4acc874b0583340a80a0aca948e7
Ammar <ammar.yassr.33@gmail.com>
2024-12-09 23:43:50 +0200

    refactor expense handling to use account-based queries and add new expense permissions

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -4,16 +4,17 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const expenseController = require("../controllers/expenseController");
 const expenseValidator = require("../utils/validators/expenseValidator");
-const { setUserId, filterUserResults } = require("../utils/requestUtils");
+const { setAccountId, filterAccountResults } = require("../utils/requestUtils");
 
 router.use(authController.protect);
 
 router
   .route("/")
-  .get(filterUserResults, expenseController.getAllExpenses)
+  .get(filterAccountResults, expenseController.getAllExpenses)
   .post(
     expenseValidator.createExpenseValidator,
-    setUserId,
+    authController.checkPermission("ADD_EXPENSE"),
+    setAccountId,
     expenseController.createExpense
   );
 
@@ -22,28 +23,33 @@ router
   .get(expenseValidator.getExpenseValidator, expenseController.getExpense)
   .patch(
     expenseValidator.updateExpenseValidator,
+    authController.checkPermission("UPDATE_EXPENSE"),
     expenseController.updateExpense
   )
   .delete(
     expenseValidator.getExpenseValidator,
+    authController.checkPermission("DELETE_EXPENSE"),
     expenseController.deleteExpense
   );
 
 router.patch(
   "/:id/pay",
   expenseValidator.payExpenseValidator,
+  authController.checkPermission("PAY_EXPENSE"),
   expenseController.payExpense
 );
 
 router.patch(
   "/:id/unpay",
   expenseValidator.getExpenseValidator,
+  authController.checkPermission("UNPAY_EXPENSE"),
   expenseController.unpayExpense
 );
 
 router.patch(
   "/:id/cancel",
   expenseValidator.getExpenseValidator,
+  authController.checkPermission("CANCEL_EXPENSE"),
   expenseController.cancelExpense
 );
 
