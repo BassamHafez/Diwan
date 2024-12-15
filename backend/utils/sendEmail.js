621112786475e1105cc77cb8490e557e797bea22
Ammar <ammar.yassr.33@gmail.com>
2024-12-15 13:20:34 +0200

    feat: add sendEmail functionality

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (email, subject, text, html = null) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.hostinger.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false, // Allow self-signed certificates (optional)
+      },
+    });
+
+    const mailOpts = {
+      from: "Diwan Amlak Website <" + process.env.EMAIL_USER + ">",
+      to: email,
+      subject,
+      text,
+      html,
+    };
+
+    const info = await transporter.sendMail(mailOpts);
+
+    console.log("Email sent successfully:", info.messageId);
+    return info; // Optional: return info for logging or debugging purposes
+  } catch (error) {
+    console.error("Error sending email:", error.message);
+
+    throw error; // Rethrow the error if you want to handle it in the calling function
+  }
+};
+
+module.exports = sendEmail;
