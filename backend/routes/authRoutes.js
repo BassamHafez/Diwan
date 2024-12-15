a70e00115a800853b1b4d0c6ac8cf976f7ebc5b9
Ammar <ammar.yassr.33@gmail.com>
2024-12-15 20:43:59 +0200

    feat: implement password reset functionality with email verification

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,20 @@ const authValidator = require("../utils/validators/authValidator");
 
 router.post("/signup", authValidator.signupValidator, authController.signup);
 router.post("/login", authValidator.loginValidator, authController.login);
+router.post(
+  "/forgotPassword",
+  authValidator.forgotPasswordValidator,
+  authController.forgotPassword
+);
+router.post(
+  "/verifyResetCode",
+  authValidator.verifyPassResetCodeValidator,
+  authController.verifyPassResetCode
+);
+router.put(
+  "/resetPassword",
+  authValidator.resetPasswordValidator,
+  authController.resetPassword
+);
 
 module.exports = router;
