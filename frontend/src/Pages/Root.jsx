8c7896c13397614f284b6ad5e168eb7a064a33c9
BassamHafez <bassamhafez790@gmail.com>
2024-12-22 21:08:47 +0200

    add overdue tasks

--- frontend/src/Pages/Root.jsx
+++ frontend/src/Pages/Root.jsx
@@ -1,23 +1,15 @@
 import { Outlet } from "react-router-dom";
 import MainNav from "../Components/MainNav/MainNav";
 import MainFooter from "../Components/Footer/MainFooter";
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
-import AdminNav from "../Components/MainNav/AdminNav";
-import LanguageChanger from "../Components/Lang/LanguageChanger";
 import ButtonOne from "../Components/UI/Buttons/ButtonOne";
 import { useTranslation } from "react-i18next";
-import LogOutModal from "../Components/UI/Modals/LogOutModal";
-import SearchField from "../Components/Search/SearchField";
 import useIsOnline from "../hooks/useIsOnline";
 import noConnectionImg from "../assets/noConnection.png";
+import AdminContainer from "./Admin/AdminsContainer/AdminContainer";
 
 const Root = () => {
-  const [logoutModalShow, setLogoutModalShow] = useState(false);
   const { t: key } = useTranslation();
-  let isArLang = localStorage.getItem("i18nextLng") === "ar";
 
   const role = useSelector((state) => state.userInfo.role);
   const isOnline = useIsOnline();
@@ -37,38 +29,7 @@ const Root = () => {
           </>
         ) : (
           <>
-            <div
-              className=" py-2 px-3"
-              style={{
-                backgroundColor: "var(--sub_white)",
-                minHeight: "100vh",
-              }}
-            >
-              <Row>
-                <Col lg={2} md={3}>
-                  <AdminNav />
-                </Col>
-                <Col lg={10} md={9}>
-                  <div className=" d-flex justify-content-between align-items-center flex-wrap mb-2 px-2">
-                    <div>
-                      <SearchField text={key("search")} />
-                    </div>
-                    <div className={`d-flex align-items-center flex-wrap ${isArLang?"me-auto":"ms-auto"}`}>
-                      <LanguageChanger />
-                      <ButtonOne
-                        onClick={() => setLogoutModalShow(true)}
-                        classes="m-2"
-                        text={key("logout")}
-                        borderd={true}
-                      />
-                    </div>
-                  </div>
-                  <div className="admin_body height_container">
-                    <Outlet />
-                  </div>
-                </Col>
-              </Row>
-            </div>
+            <AdminContainer Outlet={Outlet}/>
             <MainFooter />
           </>
         )
@@ -88,12 +49,7 @@ const Root = () => {
         </div>
       )}
 
-      {logoutModalShow && (
-        <LogOutModal
-          show={logoutModalShow}
-          onHide={() => setLogoutModalShow(false)}
-        />
-      )}
+
     </>
   );
 };
