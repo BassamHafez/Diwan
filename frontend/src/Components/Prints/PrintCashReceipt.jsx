257f17c24d2935e6956ca5141c0f8e4d160cb419
BassamHafez <bassamhafez790@gmail.com>
2024-12-24 18:46:15 +0200

    tax invoice

--- frontend/src/Components/Prints/PrintCashReceipt.jsx
+++ frontend/src/Components/Prints/PrintCashReceipt.jsx
@@ -2,29 +2,57 @@ import { useTranslation } from "react-i18next";
 import styles from "./PrintContract.module.css";
 import { formattedDate, renamedPaymentMethod } from "../Logic/LogicFun";
 import PrintHeader from "./PrintHeader";
+import MainTitle from "../UI/Words/MainTitle";
 
-const PrintCashReceipt = ({ revDetails, details, id }) => {
+const PrintCashReceipt = ({
+  revDetails,
+  estateParentCompound,
+  details,
+  id,
+  isTax,
+  taxNumber,
+}) => {
   const { t: key } = useTranslation();
   let isArLang = localStorage.getItem("i18nextLng") === "ar";
-
+  const mainTitle = isTax ? key("taxInvoice") : key("cashReceipt");
+  const calculateTotalAmount = (baseAmount) => {
+    const taxNumber = 15;
+    const baseNum = Number(baseAmount);
+    return baseNum + (taxNumber / 100) * baseNum;
+  };
   return (
     <div className={styles.container_body} id={id}>
       <PrintHeader
-        title={key("cashReceipt")}
+        title={mainTitle}
         details={details}
+        estateParentCompound={estateParentCompound}
         tenant={revDetails?.tenant}
-        partiesTitle={key("partiesCashReceipt")}
+        partiesTitle={
+          isTax ? key("partiestaxInvoice") : key("partiesCashReceipt")
+        }
       />
 
       <div className={styles.information}>
-        <h5>{key("cashRecDetails")}</h5>
+        <div className="d-flex justify-content-center">
+          <MainTitle small={true} title={mainTitle} />
+        </div>
+        {isTax && (
+          <div className={styles.tax_num}>
+            <span>
+              {key("taxNumber")} : {taxNumber}
+            </span>
+          </div>
+        )}
         <div className="scrollableTable">
           <table className={`${styles.contract_table} table`}>
             <thead className={styles.table_head}>
               <tr>
                 <th>
-                  {key("amount")} ({key("sarSmall")})
+                  {isTax
+                    ? key("baseAmount")
+                    : `${key("amount")} ${key("sarSmall")}`}
                 </th>
+                {isTax && <th>{key("total")}</th>}
                 <th>{key("dueDate")}</th>
                 <th>{key("paidAt")}</th>
                 <th>{key("paymentMethod")}</th>
@@ -33,6 +61,7 @@ const PrintCashReceipt = ({ revDetails, details, id }) => {
             <tbody className={styles.table_body}>
               <tr>
                 <td>{revDetails?.amount}</td>
+                {isTax && <td>{calculateTotalAmount(revDetails?.amount)}</td>}
                 <td>{formattedDate(revDetails?.dueDate)}</td>
                 <td>{formattedDate(revDetails?.paidAt)}</td>
                 <td>
@@ -46,14 +75,16 @@ const PrintCashReceipt = ({ revDetails, details, id }) => {
         </div>
       </div>
 
-      <div className={styles.information}>
-        <div className={styles.notes}>
-          <h5>{key("notes")}</h5>
-          <div>
-            <p>{revDetails.note ? revDetails.note : "-"}</p>
+      {revDetails.note && (
+        <div className={styles.information}>
+          <div className={styles.notes}>
+            <h5>{key("notes")}</h5>
+            <div>
+              <p>{revDetails.note ? revDetails.note : "-"}</p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- frontend/src/Components/UI/Words/MainTitle.jsx
+++ frontend/src/Components/UI/Words/MainTitle.jsx
@@ -1,8 +1,8 @@
 import styles from "./MainTitle.module.css";
 
-const MainTitle = ({title,children}) => {
+const MainTitle = ({title,children,small}) => {
   return (
-    <h5 className={styles.main_title}>
+    <h5 className={`${styles.main_title} ${small?"fs-6":""}`}>
       {title?title:children}
     </h5>
   )
